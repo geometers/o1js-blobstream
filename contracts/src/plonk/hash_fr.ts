e27124959a86f792f0e2abf2ca2d229b43316562
Andrija <akinovak@gmail.com>
2024-07-11 02:45:13 +0200

    finalize hash_fr

--- contracts/src/plonk/hash_fr.ts
+++ contracts/src/plonk/hash_fr.ts
@@ -1,5 +1,5 @@
-import { Bool, Bytes, Gadgets, Hash, Struct, UInt8 } from "o1js";
-import { FpC, FrC } from "../towers/index.js";
+import { Bool, Bytes, Gadgets, Hash, Provable, Struct, UInt8 } from "o1js";
+import { FpC, FrC, FrU } from "../towers/index.js";
 import { provableBn254BaseFieldToBytes } from "../sha/utils.js";
 
 class Bytes32 extends Bytes(32) {};
@@ -10,7 +10,7 @@ class Bytes11 extends Bytes(11) {};
 
 class BytesB0 extends Bytes(64 + 32 + 32 + 3 + 11 + 1) {}; // 0 || 0 || x || y || HASH_FR_LEN_IN_BYTES || PLONK || HASH_FR_SIZE_DOMAIN
 class BytesB1 extends Bytes(32 + 1 + 11 + 1) {}; // b0_digest || 1 || PLONK || HASH_FR_SIZE_DOMAIN
-class BytesB3 extends Bytes(32 + 1 + 11 + 1) {}; // b0_digest ^ b1_digest || 2 || PLONK || HASH_FR_SIZE_DOMAIN
+class BytesB2 extends Bytes(32 + 1 + 11 + 1) {}; // b0_digest ^ b1_digest || 2 || PLONK || HASH_FR_SIZE_DOMAIN
 
 
 // assumes that lhs and rhs are Bytes32 
@@ -42,6 +42,44 @@ function shr128(hashDigest: Bytes): FrC
     }
 
     return FrC.fromBits(shaBitRepr).assertCanonical()
+}
+
+function shl_123_modR(hashDigest: Bytes) {
+    let fields = hashDigest.toFields()
+
+    const shaBitRepr: Bool[] = []
+    let bit255 = Bool(false)
+    let bit256 = Bool(false)
+
+    for (let i = 31; i >= 0; i--) {
+        const bits = fields[i].toBits(); 
+        for (let j = 0; j < 8; j++) {
+            // we skip last 2 bits
+            if (i == 0 && j == 6) {
+                bit255 = bits[j];
+            } else if (i == 0 && j == 7) {
+                bit256 = bits[j];
+            } else {
+                shaBitRepr.push(bits[j])
+            }
+        }
+    }
+
+    const sh254 = FrC.from(7059779437489773633646340506914701874769131765994106666166191815402473914367n) // 2^254 % r
+    const sh255 = FrC.from(14119558874979547267292681013829403749538263531988213332332383630804947828734n) // 2^255 % r
+
+    let x = FrU.fromBits(shaBitRepr)
+
+    const a = Provable.if(bit255.equals(Bool(true)), FrC.provable, sh254, FrC.from(0n))
+    const b = Provable.if(bit256.equals(Bool(true)), FrC.provable, sh255, FrC.from(0n))
+
+    const SH = FrC.from(340282366920938463463374607431768211456n); // 2^128
+
+    let res = x.mul(SH).assertCanonical(); 
+    res = res.add(a.mul(SH).assertCanonical()).assertCanonical();
+    res = res.add(b.mul(SH).assertCanonical()).assertCanonical();
+
+    return res
 }
 
 
@@ -98,13 +136,12 @@ class HashFr extends Struct({
 
         bytes = bytes.concat(this.HASH_FR_SIZE_DOMAIN.bytes)
 
-        const b3 = Hash.SHA2_256.hash(new BytesB3(bytes))
+        const b2 = Hash.SHA2_256.hash(new BytesB2(bytes))
 
-        const sh = FrC.from(340282366920938463463374607431768211456n) // 2^128
-        // let res = 
-
-        let low = shr128(b3)
-        return [b0.toHex(), b1.toHex(), low.toBigInt().toString()]
+        let res = shl_123_modR(b1);
+        const low = shr128(b2); 
+        res = res.add(low).assertCanonical();
+        return [b0.toHex(), b1.toHex(), res.toBigInt().toString()]
     }
 }
 
