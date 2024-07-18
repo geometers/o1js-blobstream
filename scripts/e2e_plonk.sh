c6373d17b60871a31cbbf804f62709169a61ab1f
Kobi Gurkan <kobigurk@gmail.com>
2024-07-18 22:04:07 +0300

    make script more robust

--- scripts/e2e_plonk.sh
+++ scripts/e2e_plonk.sh
@@ -3,6 +3,29 @@
 # exit if any of scripts exit
 set -e 
 
+SCRIPT_DIR=$(dirname -- $(realpath $0)) 
+cd $SCRIPT_DIR/..
+
+rm -f mlo.json aux_wtns.json
+
+pushd ./contracts
+
+node_version=$(node -v)
+node_version=${node_version:1}
+node_version=${node_version%\.*}
+node_version=${node_version%\.*}
+node_version=$(($node_version))
+if [ $node_version -lt 22 ]
+then
+  echo "Node version is too low - $node_version. Please upgrade to NodeJS 22 or higher."
+  exit 1
+fi
+
+npm install
+npm run build
+
+popd
+
 # get aux pairing witness 
 ./scripts/get_aux_witness.sh 
 
